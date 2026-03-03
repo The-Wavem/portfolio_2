0643c3761b496f8019b5af6f706ed40d0a1a6191
agr3w <suzanakampa.sk@gmail.com>
2026-03-03 10:12:57 -0300

    ✨ feat: configurar Vite para otimização de build e adicionar arquivos de configuração do Firebase

--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,21 @@ export default defineConfig({
       "@assets": '/src/assets',
     },
   },
+  build: {
+    // Aumentamos o limite do aviso para 1MB (já que usamos libs pesadas como MUI)
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        // Ao invés de uma função complexa, passamos um objeto simples.
+        // O Vite é inteligente e vai separar apenas o que mandarmos, 
+        // e fará a limpeza automática (tree-shaking) do resto, como os ícones.
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-mui': ['@mui/material', '@emotion/react', '@emotion/styled'],
+          'vendor-firebase': ['firebase/app', 'firebase/firestore', 'firebase/auth'],
+          'vendor-motion': ['framer-motion']
+        }
+      }
+    }
+  }
 })
